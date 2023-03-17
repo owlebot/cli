cd63a7d47ceb9b5f15de12c2d3c1953b87d89b43
Khaaz <khaaz.dev@gmail.com>
2023-03-17 16:37:21 +0100

    feat: support unix environment

--- bin/init-local.sh
+++ bin/init-local.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+PACKAGE_LOCATION="@owlebot/cli"
 
 DIR="$(dirname $BASH_SOURCE)"
-DIR_ROOT="${DIR}/../"
+DIR_ROOT="$(echo "$DIR" | awk -F"node_modules" '{print $1}')node_modules";
 
 # $1 is the file to run
 # $2 is --watch or --inspect
 # -r load env var via the loadenv script
-node $2 -r "${DIR_ROOT}/src/loadenv.js" $1
+node $2 -r "${DIR_ROOT}/$PACKAGE_LOCATION/src/loadenv.js" $1
