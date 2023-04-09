6bbad511c184672deb88d409d5439b3201666949
Khaaz <khaaz.dev@gmail.com>
2023-04-09 16:27:19 +0200

    feat: proper multi platform support

--- bin/init-local.sh
+++ bin/init-local.sh
@@ -2,10 +2,20 @@
 
 PACKAGE_LOCATION="@owlebot/cli"
 
-DIR="$(dirname $BASH_SOURCE)"
-DIR_ROOT="$(echo "$DIR" | awk -F"node_modules" '{print $1}')node_modules";
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+
+# Check if the path contains "node_modules"
+if [[ "$DIR" =~ "node_modules" ]]; then
+  echo "CASE: package installed"
+  DIR_ROOT="${path%%node_modules*}"
+  DIR_ROOT="${DIR_ROOT}/node_modules/${PACKAGE_LOCATION}"
+else
+  echo "CASE: package linked"
+  DIR_ROOT=$(dirname "$DIR")
+fi
 
 # $1 is the file to run
 # $2 is --watch or --inspect
 # -r load env var via the loadenv script
-node $2 -r "${DIR_ROOT}/$PACKAGE_LOCATION/src/loadenv.js" $1
+node $2 -r "${DIR_ROOT}/src/loadenv.js" $1
