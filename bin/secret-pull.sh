930863b62d3af26b3ad03560308004715382b6de
Khaaz <khaaz.dev@gmail.com>
2023-11-11 00:01:50 +0100

    feat(secret:pull): allow providing env as param

--- bin/secret-pull.sh
+++ bin/secret-pull.sh
@@ -3,8 +3,24 @@
 PACKAGE_ROOT=$PWD
 PACKAGE_NAME=$(basename "$PACKAGE_ROOT")
 
-echo "Executing [secret:pull] for \"$PACKAGE_NAME\". Path: $PACKAGE_ROOT"
+ENV="dev"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --env)
+            ENV="$2"
+            shift 2
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+echo "Executing [secret:pull] for \"$PACKAGE_NAME\". Path: $PACKAGE_ROOT. Env: $ENV"
+
+echo $1
 
 set -x
-infisical export --path="$PACKAGE_NAME" --env=dev > $PACKAGE_ROOT/.env
+infisical export --path="$PACKAGE_NAME" --env=$ENV > $PACKAGE_ROOT/.env
 set +x
